db1052773de99a4763d96bb8c98578d422b3a4e6
EliPeixoto <eliane.peixoto@outlook.com>
2024-05-04 17:10:35 -0300

    add busca
    adicionando input que traz a oportunidade de realizar uma busca dos pensamentos atraves de palavras

--- angular-memoteca-aula-6/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular-memoteca-aula-6/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -2,6 +2,10 @@ import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 
+
+
+
+
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
@@ -12,22 +16,33 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1
   haMaisPensamentos: boolean = true;
+  filtro: string = ' ';
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(++this.paginaAtual)
+    this.service.listar(++this.paginaAtual, this.filtro)
     .subscribe(listaPensamentos =>{
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
         this.haMaisPensamentos=false
       }
     })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro)
+    .subscribe(listaPensamentos => {
+      this.listaPensamentos = listaPensamentos
+    })
+
   }
 }
 
